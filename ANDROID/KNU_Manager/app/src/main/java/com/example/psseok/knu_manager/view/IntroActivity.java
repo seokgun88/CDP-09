04c0a30da530f15c37fb49d30fd5a34e60af2488
psseok <soett55@gmail.com>
2016-04-03 20:34:11 +0900

    push KNU_Manager android app

---
+++ ANDROID/KNU_Manager/app/src/main/java/com/example/psseok/knu_manager/view/IntroActivity.java
@@ -0,0 +1,29 @@
+package com.example.psseok.knu_manager.view;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.example.psseok.knu_manager.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
