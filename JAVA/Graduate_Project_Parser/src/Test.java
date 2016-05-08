43e45e652a8a7219e64d843d6fc3a904d4532750
psseok <soett55@gmail.com>
2016-05-08 22:39:08 +0900

    divide packages

--- JAVA/Graduate_Project_Parser/src/Test.java
+++ JAVA/Graduate_Project_Parser/src/Test.java
@@ -4,6 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.jsoup.nodes.Element;
 
@@ -21,15 +22,15 @@ import parser.UserSchedulePlan;
 public class Test {
 
 	public static void main(String[] args) {
-		
-//				-----------------------------------------------
-//				[*]°øÈÞÀÏ ÆÄ½Ì
 
-				/*for(int n=1; n <= 12 ; n++){
+		//				-----------------------------------------------
+		//				[*]°øÈÞÀÏ ÆÄ½Ì
+
+		/*for(int n=1; n <= 12 ; n++){
 					System.out.println("month : " + n);
 					HolidayPlan obj = new HolidayPlan(2016,n);
 					ArrayList<HolidayObj> HolidayList = obj.getHolidayList();
-		
+
 					for(int i=0 ; i < HolidayList.size() ; i++){			
 						System.out.println("[List] "+HolidayList.get(i).toString());
 					}
@@ -38,43 +39,44 @@ public class Test {
 		//		-----------------------------------------------
 		//		[*]ÇÐ»ç ÀÏÁ¤ ÆÄ½Ì
 
-//				CollegePlan collobj = new CollegePlan(2016);
-//
-//				ArrayList<CollegeObj> CollegeList = collobj.getCollegeList();
-//				for(CollegeObj e: CollegeList){
-//					System.out.println("[List] "+ e.toString());		
-//				}
+		//				CollegePlan collobj = new CollegePlan(2016);
+		//
+		//				ArrayList<CollegeObj> CollegeList = collobj.getCollegeList();
+		//				for(CollegeObj e: CollegeList){
+		//					System.out.println("[List] "+ e.toString());		
+		//				}
 
 		//		-----------------------------------------------
 		//		[*]°­ÀÇ°èÈ¹¼­ ÆÄ½Ì
 
-				LecturePlan lp = new LecturePlan();
-				lp.TotalParse();
-				//lp.SectionParse(1);
-				ArrayList<LectureObj> LectureList = lp.getLectureList();
-				
-				for(LectureObj e: LectureList){
-					System.out.println("[List] "+ e.toString());
-				}
+//		LecturePlan lp = new LecturePlan();
+////		lp.TotalParse();
+//		lp.SectionParse(1);
+//		ArrayList<LectureObj> LectureList = lp.getLectureList();
+//
+//		for(LectureObj e: LectureList){
+//			System.out.println("[List] "+ e.toString());
+//		}
 
 		//		-----------------------------------------
 		//		[*]À¯Àú ½ºÄÉÁÙ ÆÄ½Ì(lms)
-//		String id = "aa0507a";
-//		String pw = "ss885522..";
-//		UserSchedulePlan UserTime = new UserSchedulePlan();
-//		
-//		
-//		if(UserTime.StartRequest(id, pw) == false){
-//			System.out.println("Request ½ÇÆÐ !!");
-//		}
-//		else{
-//			System.out.println("Request ¼º°ø !!");
-//			ArrayList<ScheduleAttr> attrList = UserTime.getScheduleList();
-//
-//			for(int i = 0 ;i<attrList.size();i++){			
-//				System.out.println(attrList.get(i).toString());
-//			}
-//		}
+		//		String id = "aa0507a";
+		//		String pw = "ss885522..";
+		//		UserSchedulePlan UserTime = new UserSchedulePlan();
+		//		
+		//		
+		//		if(UserTime.StartRequest(id, pw) == false){
+		//			System.out.println("Request ½ÇÆÐ !!");
+		//		}
+		//		else{
+		//			System.out.println("Request ¼º°ø !!");
+		//			ArrayList<ScheduleAttr> attrList = UserTime.getScheduleList();
+		//
+		//			for(int i = 0 ;i<attrList.size();i++){			
+		//				System.out.println(attrList.get(i).toString());
+		//			}
+		//		}
+
 
 	}
 
---
+++ WEB/second/src/main/java/com/test/second/controller/TimetableController.java
@@ -0,0 +1,62 @@
+package com.test.second.controller;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.test.second.Constant;
+import com.test.second.object.LectureObj;
+import com.test.second.parser.LecturePlan;
+
+@Controller
+public class TimetableController {
+
+	@RequestMapping("/gettimetable")
+	public String gettimetable(Model model) {
+		BufferedWriter write = null;
+
+		try {
+			write = new BufferedWriter(new FileWriter("output.txt"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		LecturePlan lp = new LecturePlan();
+		lp.TotalParse();
+		ArrayList<LectureObj> LectureList = lp.getLectureList();
+
+		for (LectureObj e : LectureList) {
+			//			System.out.println("[List] " + e.toString());
+			try {
+				write.write("[List] " + e.toString() + "\n");
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+
+		try {
+			write.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return "redirect:home";
+	}
+
+	@RequestMapping("/timetable")
+	public String timetable(Model model) {
+		//		for (int i = 0; i < Constant.scheduleList.size(); i++) {
+		//			System.out.println(Constant.scheduleList.get(i).toString());
+		//		}
+		model.addAttribute("list", Constant.scheduleList);
+		return "timetable";
+	}
+
+}
